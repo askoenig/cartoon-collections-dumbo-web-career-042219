e5881a84c6cd39a5149edd0d8ad55c5d5018c0a5
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-21 03:13:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.find(ifnone = nil) {|x| x == cheese_types}
+  snacks.find(ifnone = nil) {|x| x.include(cheese_types)}
 end
